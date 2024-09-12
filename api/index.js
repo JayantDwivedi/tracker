143b91e8348b22ac17b4dc86cdbbebb287076252
Jayant Dwivedi <49179672+JayantDwivedi@users.noreply.github.com>
2024-09-12 18:06:07 +0530

    fix: update server side.

--- api/index.js
+++ api/index.js
@@ -8,27 +8,28 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-
 app.set("view engine", "ejs");
-
-// Set the views directory to /view (if index.ejs is here)
 app.set("views", path.join(__dirname, '../view'));
-
-// Serve static files from /public (e.g., CSS, JS, images)
 app.use(express.static(path.join(__dirname, 'public')));
 
-io.on("connection",function(socket){
-    socket.on("send-location",function(data){
-        io.emit("receive-location",{id:socket.id, ...data})
-    })
+io.on("connection", function(socket) {
+    console.log(`New connection established, socket ID: ${socket.id}`);
 
-    socket.on("disconnect",function(){
-        io.emit("user-disconnected",socket.id);
-    })
-})
+    socket.on("send-location", function(data) {
+        console.log(`Location received from ${socket.id}:`, data);
+        io.emit("receive-location", { id: socket.id, ...data });
+    });
 
-app.get("/", function (req,res){
+    socket.on("disconnect", function() {
+        console.log(`User disconnected: ${socket.id}`);
+        io.emit("user-disconnected", socket.id);
+    });
+});
+
+app.get("/", function (req, res) {
     res.render("index");
-})
+});
 
-server.listen(3000);
+server.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
